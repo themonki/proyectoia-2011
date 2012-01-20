226df83c39f48c8f7f40a38522cf689ca66fc192
themonki <edgarandres29@gmail.com>
2012-01-19 23:39:10 -0500

    mejorando interfaz

--- ProyectoIA2/src/Interfaz.java
+++ ProyectoIA2/src/Interfaz.java
@@ -1,5 +1,3 @@
-package Interfaz;
-
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
@@ -22,17 +20,17 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JRadioButtonMenuItem;
-import javax.swing.JScrollPane;
 
 
 @SuppressWarnings("serial")
 public class Interfaz extends JFrame{
-	
-	JPanel panel;
+		
+	Jugar juego;
 	Color c [] = {Color.white, Color.GRAY}, colorViejo, colorSelect = Color.green;
 	Manejador manejador = new Manejador();
 	boolean seleccionado = false, flagClick = true;//nota:cambiarlo a false
 	int posSeleccionado [] = {-1,-1};
+	JPanel panel;
 	JLabel etiquetaSelect;
 	JButton botonJugadaSiguiente;
 	java.awt.Dimension d = new java.awt.Dimension(80,80);//tamaño del cuadro
@@ -51,6 +49,8 @@ public class Interfaz extends JFrame{
 	
 	public Interfaz(){
 		super();
+		juego = new Jugar();
+		tablero = juego.getTablero();
 		
 		for(int i = 0; i < 6; i ++)
 			for(int j = 0; j < 6 ; j++ )
@@ -60,14 +60,15 @@ public class Interfaz extends JFrame{
 		tablero[1][1]='Q';
 		tablero[2][0]='P';
 		tablero[2][1]='N';
-		tablero[3][2]='B';
+		tablero[3][3]='B';
 		tablero[4][4]='k';
 		tablero[5][5]='q';
-		tablero[2][5]='p';
+		tablero[2][4]='p';
 		tablero[1][4]='n';
 		tablero[0][5]='b';
 		
-		initComponet();		
+		initComponet();
+				
 	}
 		
 	public void initComponet(){
@@ -142,11 +143,8 @@ public class Interfaz extends JFrame{
 			for(int j = 0; j < 6 ; j++){
 				JLabel temp = (JLabel)(panel.getComponent(contador));
 				ImageIcon img = new ImageIcon("imagenes/"+((int)tablero[j][i])+".gif");		
-				//temp.setIcon(img);				
-				if(d.width>0 && d.height>0){
-					img.setImage(img.getImage().getScaledInstance(d.width, d.height, Image.SCALE_DEFAULT));
-					temp.setIcon(img);
-				}
+				img.setImage(img.getImage().getScaledInstance(d.width, d.height, Image.SCALE_DEFAULT));
+				temp.setIcon(img);				
 				contador++;
 			}
 		}
@@ -171,88 +169,6 @@ public class Interfaz extends JFrame{
 	}
 	
 	
-	
-	
-	/**
-	 * En el segundo click
-	 * el posxNow y el posyNow son la posicion de la ficha seleccionada (color verde)
-	 * posx y posy son la posicion donde se va a mover
-	 * pieza es la ficha seleccionada (color verde)
-	 */
-	public boolean comprobarMovimientos(char pieza, int posxNow, int posyNow, int posx, int posy ){
-		String piezaConvertida = Character.toString(pieza);
-		String elementoUbicar = Character.toString(tablero[posx][posy]);
-		String minusculas = "bknpq";
-		boolean fichaNegra= minusculas.contains(piezaConvertida);
-		boolean fichaContrariaVacia;
-		if(fichaNegra)
-		{
-			fichaContrariaVacia=!minusculas.contains(elementoUbicar) || elementoUbicar.contains(".");
-		}else{
-			fichaContrariaVacia= minusculas.contains(elementoUbicar) || elementoUbicar.contains(".");
-		}	
-		
-		if(!fichaContrariaVacia){//si donde se va a mover NO ahi una ficha contraria o un espacio vacio
-			return false;
-		}
-		int restax = Math.abs((posxNow-posx)), restay = Math.abs((posyNow-posy));
-		
-		if(pieza == 'p' || pieza == 'P'){//peon
-			
-		}else if(pieza== 'k' || pieza == 'K'){//Rey
-			if(restax<=1 && restay<=1 ){
-				return true;				
-			}
-		}else if(pieza== 'n' || pieza == 'N'){//caballo
-			if(restax==1 && restay ==2){
-				return true;
-			}else if(restax == 2 && restay == 1){
-				return true;
-			}
-		}else if(pieza== 'b' || pieza == 'B'){// alfil			
-			if(restax==restay && comprobarCaminoLibreDiagonal(posxNow, posyNow, posx, posy)){				
-				return true;
-			}
-		}else if(pieza== 'q' || pieza == 'Q'){// reina
-			if(restax==restay && comprobarCaminoLibreDiagonal(posxNow, posyNow, posx, posy)){				
-				return true;
-			}else if(restax==0 && comprobarCaminoLibreHorizontal(posxNow, posyNow, posx, posy)){
-				return true;
-			}else if(restay==0 && comprobarCaminoLibreVertical(posxNow, posyNow, posx, posy)){				
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	public boolean comprobarCaminoLibreDiagonal(int iniciox, int inicioy, int finx, int finy){
-		return comprobarCaminoLibre(iniciox, inicioy, finx, finy, 1, 1);
-	}
-	public boolean comprobarCaminoLibreHorizontal(int iniciox, int inicioy, int finx, int finy){
-		return comprobarCaminoLibre(iniciox, inicioy, finx, finy, 0, 1);
-	}
-	public boolean comprobarCaminoLibreVertical(int iniciox, int inicioy, int finx, int finy){
-		return comprobarCaminoLibre(iniciox, inicioy, finx, finy, 1, 0);
-	}
-	
-	public boolean comprobarCaminoLibre(int iniciox, int inicioy, int finx, int finy, int moverx, int movery){
-		int iteracionx =iniciox,iteraciony =inicioy, signox, signoy,divisor = Math.abs((finx-iniciox));
-		
-		if(divisor==0){signox=0;}else{signox=(finx-iniciox)/divisor;}
-		divisor = Math.abs((finy-inicioy));
-		if(divisor==0){signoy=0;}else{signoy=(finy-inicioy)/divisor;}
-		while((iteracionx!=finx || iteraciony!=finy) && 
-				(iteracionx<6 && iteraciony<6) && (iteracionx>=0 && iteraciony>=0)){			
-			if((iteracionx!=iniciox || iteraciony!=inicioy) && tablero[iteracionx][iteraciony]!='.'){
-				return false;
-			}
-			iteracionx=iteracionx + 1*moverx*signox;
-			iteraciony=iteraciony + 1*movery*signoy;
-		}	
-		return true;
-	}
-	
 	private class Manejador implements MouseListener, ActionListener{
 
 		@Override
@@ -275,7 +191,7 @@ public class Interfaz extends JFrame{
 							etiquetaSelect = null;
 						} else {
 							//no se puede mover ahi
-							if(!comprobarMovimientos(tablero[posSeleccionado[0]][posSeleccionado[1]],
+							if(!juego.comprobarMovimientos(tablero[posSeleccionado[0]][posSeleccionado[1]],
 									posSeleccionado[0],posSeleccionado[1], posx, posy)){
 								JOptionPane.showMessageDialog(null, "No se puede mover");
 								seleccionado = true;
@@ -346,6 +262,9 @@ public class Interfaz extends JFrame{
 			if(e.getSource()==botonJugadaSiguiente){
 				flagClick=true;
 				botonJugadaSiguiente.setEnabled(false);
+				juego.jugadaMax();
+				tablero=juego.getTablero();
+				cargarImagenes();			
 			}else if(e.getSource()==amateur){
 				amateur.setSelected(true);
 				principiante.setSelected(false);
